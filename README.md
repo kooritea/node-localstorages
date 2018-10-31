3e98faf43fdf3c90f67cd60e75eacadb62c06bb8
kooritea <ochatokori@gmail.com>
2018-10-31 20:08:25 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# node-storages
+
+在node 环境下简易的数据存储
+
+[![npm](https://img.shields.io/npm/v/node-storages.svg?style=flat)](https://www.npmjs.com/package/node-storages)
+
+### 为什么不使用现有的 node-storage ?
+
+因为我希望store有一个可选的超时机制
---
+++ index.js
@@ -0,0 +1,15 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.Store = factory());
+}(this, (function () {
+
+class Store {
+  constructor() {
+
+  }
+}
+
+return Store
+
+})))
